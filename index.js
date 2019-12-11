1276d3e56bc10d6f10a7a99438df28122ae217d7
github username <github email address>
2019-12-11 10:17:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+let doger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = '#a869bb';
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key == "ArrowRight"){
+    moveDodgerRight();
+  } else{
+    console.log("Invalid Key")
+  }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "")
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0 ) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
